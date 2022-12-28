ae1abc192ceee0ff2b3d4afbd691b3cc38696d37
Aniketpra123 <aniketprabhudesai12@gmail.com>
2022-12-28 22:28:59 +0530

    code

---
+++ html/javascript/getelement.js
@@ -0,0 +1,8 @@
+function changecolor(incoming){
+  var content=  document.getElementById('para');
+  content.style.color=incoming;
+}
+const res=(a,b)=>{
+    return  a+b;
+}
+console.log(res(10.20));
