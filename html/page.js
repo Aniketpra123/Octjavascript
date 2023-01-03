cff295b9274ab9120de9921a5b68b11691bc6257
Aniketpra123 <aniketprabhudesai12@gmail.com>
2023-01-03 12:35:39 +0530

    code

--- html/page.js
+++ html/page.js
@@ -1,7 +1,24 @@
-function valid(){
-  var a=document.getElementById("text1").value;
-  if(a==""){
-    document.getElementById("message").innerHTML="**please enter valid name";
+function ValidateEmail(input) {
+
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.value.match(validRegex)) {
+
+    alert("Valid email address!");
+
+    document.form1.text1.focus();
+
+    return true;
+
+  } else {
+
+    alert("Invalid email address!");
+
+    document.form1.text1.focus();
+
     return false;
+
   }
+
 }
+
