72ac1ae4afc39d7bc4bfa78a0be0210c47faa73c
Aniketpra123 <aniketprabhudesai12@gmail.com>
2023-01-04 11:55:55 +0530

    code

--- html/page.js
+++ html/page.js
@@ -1,4 +1,41 @@
-function myFunction() {
-  alert("ok");
+let username=document.getElementById("name");
+let emailId=document.getElementById("mail");
+let password=document.getElementById("password");
+let confirmPassword=document.getElementById("confirmPassword");
+let submitButton=document.getElementById("btn");
+function nameHandler(){
+if(username.value===""){
+  setErrorForInput(username,"username can't be blank");
+}else{
+  setSuccessForInput(username);
 }
+}
+function checkAll(field){
+ 
+   switch(field){
+    case "name":
+      nameHandler();
+      break;
+      case "email":
+        emailHandler();
+        break;
+        case "password":
+          passwordHandler();
+          break;
+        case "confirmPassword":
+          confirmPasswordHandler();
+          break;
+        case "submit":
+          checkAll();
 
+   }
+
+}
+submitButton.addEventListener('click',(e)=>{
+  e.preventDefault();
+  inputHandler("submit");
+})
+
+function setSuccessForInput(field){
+const formHandle=field.parentElement;
+}
