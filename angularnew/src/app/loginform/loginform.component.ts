e0cf82389267d9044315b93b3c41645e096d8447
Aniketpra123 <aniketprabhudesai12@gmail.com>
2023-01-18 22:33:37 +0530

    angularcode

--- angularnew/src/app/loginform/loginform.component.ts
+++ angularnew/src/app/loginform/loginform.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
   styleUrls: ['./loginform.component.scss']
 })
 export class LoginformComponent {
+  num=200;
   car:string="swift";
-getdata():any{
+getdata(b:any){
   return "hello frd";
 }
+booleanproperty=true;
+student={
+  name:"aniket",
+  age:25
 
 }
+item=[10,20,30]
+
+}
+
